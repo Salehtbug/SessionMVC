0066f08b478d24cb636142cc8755ca60dd0b4730
salehtbug <saleh.ababneh99@gmail.com>
2025-02-20 22:30:51 +0300

    Project3: feat(Cookies) - Added user session tracking

--- Project3/Controllers/UserController.cs
+++ Project3/Controllers/UserController.cs
@@ -56,6 +56,66 @@ namespace Project3.Controllers
             return RedirectToAction("Login");
         }
 
+
+        public IActionResult Signout()
+        {
+            foreach (var item in HttpContext.Session.Keys)
+                HttpContext.Session.Remove(item);
+
+            return RedirectToAction("Index", "Home");
+        }
+
+
+
+        [HttpPost]
+        public IActionResult SaveUser(string Name, string Email, string Password, string Re_Password)
+        {
+
+            HttpContext.Session.SetString("Name", Name);
+            HttpContext.Session.SetString("Email", Email);
+            HttpContext.Session.SetString("Password", Password);
+            HttpContext.Session.SetString("Re_Password", Re_Password);
+
+
+            return RedirectToAction("Login", "User");
+        }
+
+
+        [HttpPost]
+        public IActionResult LoginCheckUser()
+        {
+            string isChecked = Request.Form["RememberMe"].ToString();
+
+            if (isChecked == "true")
+            {
+                CookieOptions options = new CookieOptions();
+                options.Expires = DateTime.Now.AddDays(7);
+                HttpContext.Response.Cookies.Append("Email", Request.Form["EmailLogin"], options);
+
+                HttpContext.Response.Cookies.Append("Password", Request.Form["PasswordLogin"], options);
+
+            }
+
+            if (HttpContext.Session.GetString("Email") == Request.Form["EmailLogin"] &&
+                HttpContext.Session.GetString("Email") != null &&
+                HttpContext.Session.GetString("Password") == Request.Form["PasswordLogin"] &&
+                HttpContext.Session.GetString("Password") != null)
+            {
+                HttpContext.Session.SetString("signData", "Signed");
+
+                return RedirectToAction("Index", "Home");
+            }
+            else if (Request.Form["EmailLogin"] == "Admin@gmail.com" && Request.Form["PasswordLogin"] == "123321")
+            {
+                return RedirectToAction("Index", "Admin");
+            }
+            else
+                ViewBag.MSGERROR = "Your Password or Email is incorrect..";
+
+            return RedirectToAction("Login", "User");
+
+        }
+
         public IActionResult Profile()
         {
             ViewBag.Name = HttpContext.Session.GetString("Name");
